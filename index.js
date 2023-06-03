3e9d2c5ff9362004e2046735c9a1eb3224da395c
yosef langer <yosef@doar.systems>
2023-06-04 07:27:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,34 @@
 const {browser: {createBrowser}} = require('mega-scraper')
 const args = require('yargs').argv
 
+function encodeSpecialParameters(input) {
+    const specialParams = input.match(/&[a-zA-Z0-9#]+;/g);
+    if(specialParams) {
+        for(const param of specialParams) {
+            const encoded = Buffer.from(param).toString('base64');
+            const wrappedEncoded = `&nbsp;${encoded}&nbsp;`;
+            input = input.replace(new RegExp(param, 'g'), wrappedEncoded);
+            encodeMap.set(param, wrappedEncoded);
+            decodeMap.set(wrappedEncoded, param);
+        }
+    }
+    return input;
+}
+
+// Function to decode special parameters
+function decodeSpecialParameters(input) {
+    const specialParams = input.match(/&nbsp;[a-zA-Z0-9+/=]+&nbsp;/g);
+    if(specialParams) {
+        for(const param of specialParams) {
+            const original = decodeMap.get(param);
+            if(original) {
+                input = input.replace(new RegExp(param, 'g'), original);
+            }
+        }
+    }
+    return input;
+}
+
 async function translate({text, from = 'auto', to = 'en', browser, page} = {}) {
     if (!text) throw new Error('missing text')
     if (!from) throw new Error('missing from')
@@ -9,14 +37,14 @@ async function translate({text, from = 'auto', to = 'en', browser, page} = {}) {
     if (!browser) browser = await createBrowser({headless: true, screenshot: true})
 
     page = page || await browser.newPage()
-    await page.goto(`https://translate.google.com/?op=translate&sl=${from}&tl=${to}&text=${encodeURIComponent(text)}`)
+    await page.goto(`https://translate.google.com/?op=translate&sl=${from}&tl=${to}&text=${encodeURIComponent(encodeSpecialParameters(text))}`)
     await page.waitForSelector('[jsaction^="copy:"] div span[lang="ko"] span[jsaction^="click:"]', {timeout: 10000})
 
     const translation = await page.evaluate(() => [...document.querySelectorAll('[jsaction^="copy:"] div span[lang="ko"] span[jsaction^="click:"]')].map(e => e.textContent.trim()).filter(s => s !== 'Try again').join(' '))
     await page.close()
     await browser.instance.close()
 
-    return translation
+    return decodeSpecialParameters(translation)
 }
 
 module.exports = translate
