928f5d26c51bfe1ba0454b14a8bf729b701bc131
yosef langer <yosef@doar.systems>
2023-06-04 05:47:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 #!/usr/bin/env node
 const { browser: { createBrowser } } = require('mega-scraper')
 const args = require('yargs').argv
-
-if (require.main === module) {
-  translate(args)
-    .then(translation => {
-      console.log(translation)
-      process.exit(0)
-    })
-    .catch(err => {
-      console.error(err)
-      process.exit(1)
-    })
-} else {
-  module.exports = translate
-}
-
 async function translate ({ text, from = 'auto', to = 'en', browser, page } = {}) {
   if (!text) throw new Error('missing text')
   if (!from) throw new Error('missing from')
   if (!to) throw new Error('missing to')
-  if (!browser) browser = await createBrowser({ headless: true })
+  if (!browser) browser = await createBrowser({ headless: true, screenshot:true })
 
   page = page || await browser.newPage()
   await page.goto(`https://translate.google.com/#view=home&op=translate&sl=${from}&tl=${to}&text=${encodeURIComponent(text)}`)
